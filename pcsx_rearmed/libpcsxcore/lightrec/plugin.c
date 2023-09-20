6757b0fbd64c6419d0633844b3241ef92dfc5946
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2023-09-20 07:39:06 -0500

    lightrec: Emulate BREAK (TEST)

--- pcsx_rearmed/libpcsxcore/lightrec/plugin.c
+++ pcsx_rearmed/libpcsxcore/lightrec/plugin.c
@@ -528,6 +528,8 @@ static void lightrec_plugin_execute_internal(bool block_only)
 
 		if (flags & LIGHTREC_EXIT_SYSCALL)
 			psxException(0x20, 0, (psxCP0Regs *)regs->cp0);
+		if (flags & LIGHTREC_EXIT_BREAK)
+			psxException(0x20, 0, (psxCP0Regs *)regs->cp0);
 	}
 
 	if ((regs->cp0[13] & regs->cp0[12] & 0x300) && (regs->cp0[12] & 0x1)) {
